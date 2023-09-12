d56b75b2fbf66681a2fa4f675039561e80aa585b
Paras Arya <parasarya289@gmail.com>
2023-09-13 00:21:05 +0530

    feat: router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import {Route, Routes } from "react-router";
 import "./App.css";
+import { Home } from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>This is component library</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
