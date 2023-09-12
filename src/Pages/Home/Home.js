75b5475e6a08ce85e4601e4f1a6358d0da221e10
Paras Arya <parasarya289@gmail.com>
2023-09-13 02:12:06 +0530

    style: scroll button

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,27 @@
+import { BsChevronDoubleDown } from "react-icons/bs";
 import "./Home.css";
-export const Home = () =>{
-    return (
-        <main>
-            <section className="page1">
-                section 1
-            </section>
-            <section className="page2">
-                section 2
-            </section>
-        </main>
-    )
-}
+export const Home = () => {
+  return (
+    <main>
+      <section className="page1">
+        <h1>Arctic UI</h1>
+        <p>
+          Core building blocks
+          <br /> for your design system
+        </p>
+        <p>
+          Unstyled, accessible, open source React primitives
+          <br /> for high-quality web apps and design systems.
+        </p>
+        <div>
+          <button>Get started</button>
+          <button>Preview</button>
+        </div>
+        <button>
+          <BsChevronDoubleDown />
+        </button>
+      </section>
+      <section className="page2"></section>
+    </main>
+  );
+};
