15f5ecc787ae4a2102b7342259da4c3a8fc67a36
Paras Arya <parasarya289@gmail.com>
2023-09-13 03:09:37 +0530

    feat: navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import {Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import "./App.css";
+import { Navbar } from "./Components/Navbar/Navbar";
 import { Home } from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
