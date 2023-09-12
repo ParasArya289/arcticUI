dfa8942e184c7ce304b7cb15586f8fab1c468bb4
Paras Arya <parasarya289@gmail.com>
2023-09-13 00:19:19 +0530

    feat: basic home page

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+export const Home = () =>{
+    return (
+        <main>
+            <section>
+                section 1
+            </section>
+            <section>
+                section 2
+            </section>
+        </main>
+    )
+}
