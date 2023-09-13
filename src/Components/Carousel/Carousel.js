828a9afbdbdd2873172af03005045fa643619be4
Paras Arya <parasarya289@gmail.com>
2023-09-13 15:45:27 +0530

    feat: carousel

---
+++ src/Components/Carousel/Carousel.js
@@ -0,0 +1,47 @@
+import { useRef, useState } from "react";
+import { ComponentCard } from "../ComponentCard/ComponentCard";
+import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
+import "./Carousel.css";
+
+export const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselRef = useRef(null);
+  const components = ["one", "two", "thhree", "four", "five"];
+
+  const handleNext = () => {
+    if (currentIndex < components.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const itemWidth = carouselRef.current
+    ? carouselRef.current.clientWidth / components.length
+    : 0;
+
+  const transformValue = `translateX(-${400 * currentIndex}px)`;
+  return (
+    <div className="carousel">
+      <button onClick={handlePrevious}>
+        <BsChevronLeft />
+      </button>
+      <div
+        className="list"
+        ref={carouselRef}
+        style={{ transform: transformValue }}
+      >
+        {components.map((component) => (
+          <ComponentCard component={component} />
+        ))}
+      </div>
+      <button onClick={handleNext}>
+        <BsChevronRight />
+      </button>
+    </div>
+  );
+};
