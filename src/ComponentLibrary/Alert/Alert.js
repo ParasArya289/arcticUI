e914080e1f5fa8715b4431e440c3e0e2555e32ba
Paras Arya <parasarya289@gmail.com>
2023-09-13 17:02:17 +0530

    feat: icons for alert variants

--- src/ComponentLibrary/Alert/Alert.js
+++ src/ComponentLibrary/Alert/Alert.js
@@ -1,6 +1,23 @@
 import "./Alert.css";
 import styles from "../../global.module.css";
+import {
+  AiOutlineInfoCircle,
+  AiOutlineCheckCircle,
+  AiOutlineWarning,
+} from "react-icons/ai";
+import { RxCrossCircled } from "react-icons/rx";
 
 export const Alert = ({ variant, text }) => {
-  return <div className={`alert ${styles[variant]}`}>{<p>{text}</p>}</div>;
+  const alertIcons = {
+    info: <AiOutlineInfoCircle />,
+    success: <AiOutlineCheckCircle />,
+    warning: <AiOutlineWarning />,
+    danger: <RxCrossCircled />,
+  };
+  return (
+    <div className={`alert ${styles[variant]}`}>
+      <span>{alertIcons[variant]}</span>
+      <p>{text}</p>
+    </div>
+  );
 };
