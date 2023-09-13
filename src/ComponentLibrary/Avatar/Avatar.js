a2c3db02b6a9f5ee5c85f8def4b2b785b52ad870
Paras Arya <parasarya289@gmail.com>
2023-09-13 18:10:08 +0530

    feat: avatar

---
+++ src/ComponentLibrary/Avatar/Avatar.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./Avatar.css";
+
+export const Avatar = ({ src, name, fallback, size = "md" }) => {
+  const [loading, setLoading] = useState(true);
+
+  const avatarSize = {
+    sm: 44,
+    md: 66,
+    lg: 88,
+  };
+
+  const imageLoadHandler = () => setLoading(false);
+
+  return (
+    <div
+      className="avatar"
+      style={{
+        width: `${avatarSize[size]}px`,
+        height: `${avatarSize[size]}px`,
+      }}
+    >
+      <img
+        style={{ display: !src || loading ? "none" : "" }}
+        draggable={false}
+        src={src}
+        alt={name}
+        onLoad={imageLoadHandler}
+        height={avatarSize[size]}
+      />
+      {(!src || loading) && <p>{fallback}</p>}
+    </div>
+  );
+};
