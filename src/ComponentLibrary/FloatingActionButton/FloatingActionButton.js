758ab4de3215eb5a4c039c28bcd026245353c5d3
Paras Arya <parasarya289@gmail.com>
2023-09-14 12:29:03 +0530

    fix: icon and text collision

---
+++ src/ComponentLibrary/FloatingActionButton/FloatingActionButton.js
@@ -0,0 +1,31 @@
+import FAB_style from "./FloatingActionButton.module.css";
+import styles from "../../global.module.css";
+export const FloatingActionButton = ({
+  text,
+  top,
+  bottom,
+  left,
+  right,
+  translateX,
+  translateY,
+  className,
+  children,
+  size = "md",
+  variant = "dark",
+}) => {
+  return (
+    <button
+      className={`${FAB_style["floatingActionButton"]} ${FAB_style[size]} ${className} ${styles[variant]}`}
+      style={{
+        top,
+        bottom,
+        left,
+        right,
+        transform: `translate(${translateX}, ${translateY})`,
+      }}
+    >
+      {children && <span>{children}</span>}
+      {text && <p>{text}</p>}
+    </button>
+  );
+};
