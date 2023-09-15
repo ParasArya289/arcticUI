c622bf1c1ed23e3bb34028bbff813fb57f66e135
Paras Arya <parasarya289@gmail.com>
2023-09-16 00:41:20 +0530

    fix: alert to be fit content

--- src/Pages/Docs/Docs.js
+++ src/Pages/Docs/Docs.js
@@ -1,49 +1,17 @@
+import { useParams } from "react-router-dom";
 import { Heading } from "../../ComponentLibrary/Heading/Heading";
 import { Layout } from "../../Components/Layout/Layout";
+import { components } from "../../Utils";
 import "./Docs.css";
 
 export const Docs = () => {
+  const { componentName } = useParams();
+
+  const component = components.find(({ name }) => name === componentName);
   return (
     <Layout>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
-      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is {componentName}</Heading>
+      {component.component}
     </Layout>
   );
 };
