d74cd2b8cba5a3a4b83f938a49634c7f1fdd6e17
Paras Arya <parasarya289@gmail.com>
2023-09-15 12:51:08 +0530

    feat: ability to navigate, and name of the component

--- src/Components/ComponentCard/ComponentCard.js
+++ src/Components/ComponentCard/ComponentCard.js
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { Text } from "../../ComponentLibrary/Text/Text";
 import "./ComponentCard.css";
 
-export const ComponentCard = ({component}) => {
+export const ComponentCard = ({ component }) => {
+  const navigate = useNavigate();
+  const navigateHandler = (e) => {
+    e.stopPropagation();
+    navigate("./components/" + component.name);
+  };
   return (
-    <div className="componentCard">
-      {component}
+    <div>
+      <div className="componentCard" onClick={navigateHandler}>
+        {component.component}
+      </div>
+      <Text textAlign={"left"} size="medium">
+        {component.name}
+      </Text>
     </div>
   );
 };
