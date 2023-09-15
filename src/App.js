a881d8bc3d10b3761cf487ef045cfd0d075582ed
Paras Arya <parasarya289@gmail.com>
2023-09-16 00:24:59 +0530

    feat: changed in route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Components } from "./Pages/Components/Components";
+import { Docs } from "./Pages/Docs/Docs";
 import { Home } from "./Pages/Home/Home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/components" element={<Components />} />
+        <Route path="/docs/:componentName" element={<Docs />} />
       </Routes>
     </div>
   );
