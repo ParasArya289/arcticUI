d679d106e1186d5425849cf420c6a2c94d19cc3f
Paras Arya <parasarya289@gmail.com>
2023-09-15 12:50:23 +0530

    fix: separate properties for name and component

--- src/Utils.js
+++ src/Utils.js
@@ -13,80 +13,123 @@ import { TextOverlay } from "./ComponentLibrary/TextOverlay/TextOverlay";
 import { HorizontalCard } from "./ComponentLibrary/HorizontalCard/HorizontalCard";
 
 export const components = [
-  <Alert variant="info" text="Oops something went wrong" />,
-  <Avatar
-    fallback="D"
-    size="lg"
-    name="david"
-    src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  />,
-  <Avatar
-    fallback="D"
-    size="lg"
-    name="david"
-    src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  >
-    <Badge variant="info" size="sm" text="4+" />
-  </Avatar>,
-  <Button text="Button" size="md" variant="info">
-    <HiOutlineSquare2Stack />
-  </Button>,
-  <FloatingActionButton
-    // text="Add"
-    top="50%"
-    left="50%"
-    size="lg"
-    variant="info"
-    translateX="-50%"
-    translateY="-50%"
-  >
-    <FiFolderPlus />
-  </FloatingActionButton>,
-  <Card variant="dark">
-    <Image
-      src={
-        "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      }
-      fallback={
-        "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
-      }
-      alt="arctic"
-      borderRadius={"4px 4px 0 0 "}
-      aspectRatio={"16/9"}
-    />
-    <Heading size="large" textAlign="left">
-      Arctic UI
-    </Heading>
-    <Text size="medium" textAlign="left">
-      Core building blocks for your design system
-    </Text>
-    <Button text="Check out" size="md" variant="info" />
-  </Card>,
-  <TextOverlay src="https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
-    <Heading size="large" textAlign="left">
-      Arctic UI
-    </Heading>
-    <Text size="medium" textAlign="left">
-      Core building blocks for your design system
-    </Text>
-    <Button text="Check out" size="md" variant="info" />
-  </TextOverlay>,
-  <HorizontalCard
-    src="https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    variant="dark"
-  >
-    <Heading size="medium" textAlign="left">
-      Arctic UI
-    </Heading>
-    <Text size="smaller" textAlign="left">
-      Core building blocks for your design system
-    </Text>
-  </HorizontalCard>,
-  <Image
-    src="https://images.pexels.com/photos/1009136/pexels-photo-1009136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    alt="arctic"
-    height="286"
-    width="180"
-    rounded
-  />,
+  {
+    name: "Alert",
+    component: <Alert variant="info" text="Oops something went wrong" />,
+  },
+  {
+    name: "Avatar",
+    component: (
+      <Avatar
+        fallback="D"
+        size="lg"
+        name="david"
+        src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      />
+    ),
+  },
+  {
+    name: "Badge",
+    component: (
+      <Avatar
+        fallback="D"
+        size="lg"
+        name="david"
+        src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      >
+        <Badge variant="info" size="sm" text="4+" />
+      </Avatar>
+    ),
+  },
+  {
+    name: "Button",
+    component: (
+      <Button text="Button" size="md" variant="info">
+        <HiOutlineSquare2Stack />
+      </Button>
+    ),
+  },
+  {
+    name: "Floating Action Button",
+    component: (
+      <FloatingActionButton
+        // text="Add"
+        top="50%"
+        left="50%"
+        size="lg"
+        variant="info"
+        translateX="-50%"
+        translateY="-50%"
+      >
+        <FiFolderPlus />
+      </FloatingActionButton>
+    ),
+  },
+  {
+    name: "Card",
+    component: (
+      <Card variant="dark">
+        <Image
+          src={
+            "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          }
+          fallback={
+            "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
+          }
+          alt="arctic"
+          borderRadius={"4px 4px 0 0 "}
+          aspectRatio={"16/9"}
+        />
+        <Heading size="large" textAlign="left">
+          Arctic UI
+        </Heading>
+        <Text size="medium" textAlign="left">
+          Core building blocks for your design system
+        </Text>
+        <Button text="Check out" size="md" variant="info" />
+      </Card>
+    ),
+  },
+  {
+    name: "Text Overlay",
+    component: (
+      <TextOverlay src="https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+        <Heading size="large" textAlign="left">
+          Arctic UI
+        </Heading>
+        <Text size="medium" textAlign="left">
+          Core building blocks for your design system
+        </Text>
+        <Button text="Check out" size="md" variant="info" />
+      </TextOverlay>
+    ),
+  },
+  {
+    name: "Horizontal Card",
+    component: (
+      <HorizontalCard
+        src="https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        variant="dark"
+      >
+        <Heading size="medium" textAlign="left">
+          Arctic UI
+        </Heading>
+        <Text size="smaller" textAlign="left">
+          Core building blocks for your design system
+        </Text>
+      </HorizontalCard>
+    ),
+  },
+  {
+    name: "Image",
+    component: (
+      <Image
+        src="https://images.pexels.com/photos/1009136/pexels-photo-1009136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="arctic"
+        height="286"
+        width="180"
+        rounded
+      />
+    ),
+  },
 ];
