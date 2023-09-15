7c3070b4509041c4de4a76589b5e6c9b61493e5a
Paras Arya <parasarya289@gmail.com>
2023-09-16 00:26:32 +0530

    feat: docs page

---
+++ src/Pages/Docs/Docs.js
@@ -0,0 +1,49 @@
+import { Heading } from "../../ComponentLibrary/Heading/Heading";
+import { Layout } from "../../Components/Layout/Layout";
+import "./Docs.css";
+
+export const Docs = () => {
+  return (
+    <Layout>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+      <Heading size={"large"}>This is docs</Heading>
+    </Layout>
+  );
+};
