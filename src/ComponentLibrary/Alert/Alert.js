348b80ca90a3ca9f9d7665bdbedd15d562069358
Paras Arya <parasarya289@gmail.com>
2023-11-21 01:42:32 +0530

    feat: dismiss feature for alert

--- src/ComponentLibrary/Alert/Alert.js
+++ src/ComponentLibrary/Alert/Alert.js
@@ -5,19 +5,41 @@ import {
   AiOutlineCheckCircle,
   AiOutlineWarning,
 } from "react-icons/ai";
-import { RxCrossCircled } from "react-icons/rx";
+import { RxCross2, RxCrossCircled } from "react-icons/rx";
+import { useEffect, useRef, useState } from "react";
 
-export const Alert = ({ variant, text }) => {
+export const Alert = ({ variant, text, dismissable=true }) => {
+  const [dismiss, setDismiss] = useState(false);
+  const [animateOut, setAnimateOut] = useState(false);
+  const alertRef = useRef(null);
   const alertIcons = {
     info: <AiOutlineInfoCircle />,
     success: <AiOutlineCheckCircle />,
     warning: <AiOutlineWarning />,
     danger: <RxCrossCircled />,
   };
+  useEffect(() => {
+    if (dismissable) {
+      alertRef?.current?.setAttribute("data-dismiss", dismiss);
+    }
+    const timeoutId = setTimeout(() => {
+      if (dismiss === true) {
+        setAnimateOut(true);
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [dismiss]);
   return (
-    <div className={`alert ${styles[variant]}`}>
-      <span>{alertIcons[variant]}</span>
-      <p>{text}</p>
-    </div>
+    <>
+      {!animateOut && (
+        <div ref={alertRef} className={`alert ${styles[variant]}`}>
+          <span>{alertIcons[variant]}</span>
+          <p>{text}</p>
+          {dismissable && <RxCross2 className="alert__dismiss" onClick={() => setDismiss(true)} />}
+        </div>
+      )}
+    </>
   );
 };
