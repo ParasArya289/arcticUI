3134a341d69655b44711a10fbc9b6fb614570aea
Paras Arya <parasarya289@gmail.com>
2023-11-20 22:38:43 +0530

    feat: doc update for card dimiss and animate out

--- src/Pages/Docs/CardDoc/CardDoc.js
+++ src/Pages/Docs/CardDoc/CardDoc.js
@@ -33,6 +33,31 @@ export const CardDoc = () => {
   
   
   `;
+  const codeString2 = ` 
+  
+  
+  <Card variant="dark" dismissable aniamteOut>
+    <Image
+        src="imgSrcUrl"
+        fallback="imgSrcUrl"
+        alt="arctic"
+        borderRadius={"4px 4px 0 0 "}
+        aspectRatio={"16/9"}
+    />
+
+    <Heading size="large" textAlign="left">
+        Arctic UI
+    </Heading>
+
+    <Text size="medium" textAlign="left">
+        Core building blocks for your design system
+    </Text>
+
+    <Button text="Check out" size="md" variant="info" />
+  </Card>
+  
+  
+  `;
   return (
     <Layout>
       <div className="docs__main">
@@ -173,6 +198,145 @@ export const CardDoc = () => {
           <Heading size={"large"}>How to use</Heading>
           <SyntaxHighlighter language="jsx" style={nightOwl}>
             {codeString}
+          </SyntaxHighlighter>
+        </div>
+        <Heading size="large">Card with dismiss</Heading>
+        <p className="docs__description">
+          By setting "dismissable" prop to true card will become
+          dismissable. Setting "animateOut" prop to true will make card to animate out on dismiss.
+        </p>
+
+        <div className="docs__preview" style={{ gap: "30px" }}>
+          <Card dismissable animateOut variant="info">
+            <Image
+              src={
+                "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              fallback={
+                "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
+              }
+              alt="arctic"
+              borderRadius={"4px 4px 0 0 "}
+              aspectRatio={"16/9"}
+            />
+            <Heading size="large" textAlign="left">
+              Arctic UI
+            </Heading>
+            <Text size="medium" textAlign="left">
+              Core building blocks for your design system
+            </Text>
+            <Button text="Check out" size="md" variant="dark" />
+          </Card>
+
+          <Card dismissable animateOut variant="success">
+            <Image
+              src={
+                "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              fallback={
+                "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
+              }
+              alt="arctic"
+              borderRadius={"4px 4px 0 0 "}
+              aspectRatio={"16/9"}
+            />
+            <Heading size="large" textAlign="left">
+              Arctic UI
+            </Heading>
+            <Text size="medium" textAlign="left">
+              Core building blocks for your design system
+            </Text>
+            <Button text="Check out" size="md" variant="warning" />
+          </Card>
+
+          <Card dismissable animateOut variant="warning">
+            <Image
+              src={
+                "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              fallback={
+                "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
+              }
+              alt="arctic"
+              borderRadius={"4px 4px 0 0 "}
+              aspectRatio={"16/9"}
+            />
+            <Heading size="large" textAlign="left">
+              Arctic UI
+            </Heading>
+            <Text size="medium" textAlign="left">
+              Core building blocks for your design system
+            </Text>
+            <Button text="Check out" size="md" variant="success" />
+          </Card>
+
+          <Card dismissable animateOut variant="danger">
+            <Image
+              src={
+                "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              fallback={
+                "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
+              }
+              alt="arctic"
+              borderRadius={"4px 4px 0 0 "}
+              aspectRatio={"16/9"}
+            />
+            <Heading size="large" textAlign="left">
+              Arctic UI
+            </Heading>
+            <Text size="medium" textAlign="left">
+              Core building blocks for your design system
+            </Text>
+            <Button text="Check out" size="md" variant="light" />
+          </Card>
+
+          <Card dismissable animateOut variant="light">
+            <Image
+              src={
+                "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              fallback={
+                "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
+              }
+              alt="arctic"
+              borderRadius={"4px 4px 0 0 "}
+              aspectRatio={"16/9"}
+            />
+            <Heading size="large" textAlign="left">
+              Arctic UI
+            </Heading>
+            <Text size="medium" textAlign="left">
+              Core building blocks for your design system
+            </Text>
+            <Button text="Check out" size="md" variant="danger" />
+          </Card>
+
+          <Card dismissable animateOut variant="dark">
+            <Image
+              src={
+                "https://images.pexels.com/photos/1647220/pexels-photo-1647220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              fallback={
+                "https://zhulian.com/wp-content/uploads/2023/03/fallback-image.webp"
+              }
+              alt="arctic"
+              borderRadius={"4px 4px 0 0 "}
+              aspectRatio={"16/9"}
+            />
+            <Heading size="large" textAlign="left">
+              Arctic UI
+            </Heading>
+            <Text size="medium" textAlign="left">
+              Core building blocks for your design system
+            </Text>
+            <Button text="Check out" size="md" variant="info" />
+          </Card>
+        </div>
+        <div className="docs__demo">
+          <Heading size={"large"}>How to use</Heading>
+          <SyntaxHighlighter language="jsx" style={nightOwl}>
+            {codeString2}
           </SyntaxHighlighter>
         </div>
       </div>
